ecae362f56fa1aab282ac48ab4afe95a029ab491
Mathilde H <76396548+MathildeHmn@users.noreply.github.com>
2021-01-13 19:43:24 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-/* SHOW MENU */
+// Show menu
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) => {
 }
 showMenu('nav-toggle', 'nav-menu');
 
-/* SHOW AND REMOVE MENU */
+// Show and remove menu
 const navLink = document.querySelectorAll('.nav-link');
 
 function linkAction() {
@@ -24,7 +24,7 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/* SCROLL REVEAL ANIMATION */
+// Scroll reveal animation
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -32,25 +32,25 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/* SCROLL HOME */
+// Home
 sr.reveal('.home-title',{});
 sr.reveal('.button',{delay: 200});
 sr.reveal('.home-img',{delay: 400});
 sr.reveal('.home-social-icon',{interval: 200});
 
-/* SCROLL ABOUT */
+// About
 sr.reveal('.about-img',{});
 sr.reveal('.about-subtitle',{delay: 200});
 sr.reveal('.about-text',{delay: 400});
 
-/* SCROLL SKILLS */
+// Skills
 sr.reveal('.skills-subtitle',{});
 sr.reveal('.skills-text',{delay: 200});
 sr.reveal('.skills-data',{interval: 200});
 sr.reveal('.skills-img',{delay: 400});
 
-/* SCROLL WORK */
+// Work
 sr.reveal('.work-img',{interval: 200});
 
-/* SCROLL CONTACT */
+// Contact
 sr.reveal('.contact-input',{interval: 200});
